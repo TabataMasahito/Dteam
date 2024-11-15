d41f6fd896c28b080cecf696ba7867eeefe5f41d
Tabata Masahito <fki2366213@stu.o-hara.ac.jp>
2024-11-15 15:41:15 +0900

    ログイン必須

--- apps/exercise/views.py
+++ apps/exercise/views.py
@@ -2,16 +2,18 @@ from flask import Blueprint, render_template
 from apps.exercise.forms import ModeSelectForm,WeightRecordForm,GoolSettingForm
 from flask import Blueprint, render_template, flash, url_for, redirect, request,session
 
-from flask_login import login_user,logout_user
+from flask_login import login_user,logout_user,login_required
 
 # template_folderを指定する（staticは指定しない）
 dt = Blueprint("exercise", __name__, template_folder="templates")
 # dtアプリケーションを使ってエンドポイントを作成する
 @dt.route("/")
+@login_required
 def index():
     return render_template("exercise/index.html")
 
 @dt.route("/modeselect", methods=["GET", "POST"])
+@login_required
 def mode_select():
     form = ModeSelectForm()
     if form.validate_on_submit():
@@ -23,11 +25,13 @@ def mode_select():
     return render_template("exercise/modeselect.html", form=form)
 
 @dt.route("/weightrecord")
+@login_required
 def weight_record():
     form = WeightRecordForm()
     return render_template("exercise/weight_record.html", form=form)
 
 @dt.route("/goalsetting")
+@login_required
 def goal_setting():
     form = GoolSettingForm()
     return render_template("exercise/goal_setting.html", form=form)
