51a8c2d7de96e4d819ad05a93a407ae5bf21e558
Tabata Masahito <fki2366213@stu.o-hara.ac.jp>
2024-11-18 12:51:21 +0900

    プラン設計機能仮

--- apps/exercise/views.py
+++ apps/exercise/views.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
 from apps.exercise.forms import ModeSelectForm,WeightRecordForm,GoolSettingForm
 from flask import Blueprint, render_template, flash, url_for, redirect, request,session
-
-from flask_login import login_user,logout_user,login_required
+from apps.exercise.models import WeightRecord,ExercisePlan
+from apps.app import db
+from flask_login import login_user,logout_user,login_required,current_user
 
 # template_folderを指定する（staticは指定しない）
 dt = Blueprint("exercise", __name__, template_folder="templates")
@@ -17,12 +18,46 @@ def index():
 def mode_select():
     form = ModeSelectForm()
     if form.validate_on_submit():
-        # ユーザー情報を取得
-
-        # 身体情報入力画面へリダイレクト
+        mode = form.mode.data
+        session['mode'] = mode
         return redirect(url_for("exercise.goal_setting"))
 
     return render_template("exercise/modeselect.html", form=form)
+
+@dt.route("/goalsetting", methods=["GET", "POST"])
+@login_required
+def goal_setting():
+    form = GoolSettingForm()
+    if form.validate_on_submit():
+        mode = session.get('mode')
+        getid=current_user.id
+        # DB内で同じuser_idを持つレコードの数を取得
+        count = WeightRecord.query.filter_by(user_id=getid).count()
+        # 新しいidを生成 (renameにcountを結合)
+        new_id = getid+"plan"+count + 1
+        exerciseplan = WeightRecord(
+            id=new_id,
+            user_id=getid,
+            mode=mode,
+            period=form.period.data,
+            goalweight=form.goalweight.data,
+            numberexercisesweek=form.numberexercisesweek.data
+        )
+        db.session.add(exerciseplan)
+        db.session.commit()
+        return redirect(url_for("exercise.paln_setting_complete"))
+
+        
+
+    return render_template("exercise/goal_setting.html", form=form)
+
+@dt.route("/plansettingconplete")
+@login_required
+def paln_setting_complete():
+    return render_template("exercise.paln_setting_complete.html")
+
+
+
 
 @dt.route("/weightrecord")
 @login_required
@@ -30,13 +65,11 @@ def weight_record():
     form = WeightRecordForm()
     return render_template("exercise/weight_record.html", form=form)
 
-@dt.route("/goalsetting")
-@login_required
-def goal_setting():
-    form = GoolSettingForm()
-    return render_template("exercise/goal_setting.html", form=form)
+
 
 @dt.route("/confirmyourgoal")
 @login_required
 def confirm_your_goal():
     return render_template("exercise/confirm_your_goal.html")
+
+
