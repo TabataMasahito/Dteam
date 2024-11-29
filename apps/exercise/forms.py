cb5d01a4f21d128af973e15cad1a40321406b9f9
Tabata Masahito <fki2366213@stu.o-hara.ac.jp>
2024-11-29 12:40:38 +0900

    エラー画面のCSS

--- apps/exercise/forms.py
+++ apps/exercise/forms.py
@@ -8,7 +8,7 @@ class ModeSelectForm(FlaskForm):
         ('筋トレ', '筋トレモード'),
         ('ダイエット', 'ダイエットモード'),
         ('健康維持', '健康維持モード')
-    ], validators=[DataRequired('モードを選択してください。')
+    ], validators=[DataRequired('この項目は必須です')
     ])
     submit = SubmitField('次へ')
 
@@ -16,20 +16,20 @@ class GoolSettingForm(FlaskForm):
     period = IntegerField(
         "期間",
         validators=[
-            DataRequired(message="すべての項目を入力してください")
+            DataRequired(message="この項目は必須です。")
         ]
     )
     goalweight = FloatField(
         "目標体重",
         validators=[
-            DataRequired(message="体重は必須です。"),
+            DataRequired(message="この項目は必須です。"),
             NumberRange(1,150,message="正しい数字を入力してください。")
         ]
     )
     numberexercisesweek = IntegerField(
         "週の運動回数",
         validators=[
-            DataRequired(message="体重は必須です。"),
+            DataRequired(message="この項目は必須です。"),
             NumberRange(1,7,message="正しい数字を入力してください。")
         ]
         
